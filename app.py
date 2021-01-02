9e36ad432eda27f018a27ef89b0f0299829f5869
KonstIT <51171188+KonstIT@users.noreply.github.com>
2021-01-02 13:23:04 +0100

    Dashboard Basic Setup  #1Start

---
+++ app.py
@@ -0,0 +1,91 @@
+import dash
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+from dash.dependencies import Input, Output
+from home import home_updateLayout
+from page1 import p1_updateLayout
+from page2 import p2_updateLayout
+from page3 import p3_updateLayout
+from page4 import p4_updateLayout
+from page5 import p5_updateLayout
+
+#Inititalise app    and it's style for the theme
+app = dash.Dash(external_stylesheets=[dbc.themes.LUX])
+
+
+# the style arguments for the sidebar. We use position:fixed and a fixed width
+SIDEBAR_STYLE = {
+    "display" : "inline-block",
+    "float" : "left",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "16rem",
+    "height": "100vh",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
+}
+
+# the styles for the main content position it to the right of the sidebar and
+CONTENT_STYLE = {
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+}
+#Sidebar conaining the menu
+sidebar = html.Div(
+    [
+        html.H2("Name", className="display-4"),
+        html.Hr(),
+        html.P(
+            "Explanation", className="lead"
+        ),
+
+        #Navbar containing the menu list
+        dbc.Nav(
+            [
+                dbc.NavLink("Home", href="/", active="exact"),
+                dbc.NavLink("Topic 1", href="/page1", active="exact"),
+                dbc.NavLink("Topic 2", href="/page2", active="exact"),
+                dbc.NavLink("Topic 3", href="/page3", active="exact"),
+                dbc.NavLink("Topic 4", href="/page4", active="exact"),
+                dbc.NavLink("Topic 5", href="/page5", active="exact"),
+            ],
+            vertical=True,
+            pills=True,
+        ),
+    ],
+    style=SIDEBAR_STYLE,
+)
+#Footer
+footer = dbc.Container(html.Div(dbc.Container("Footer", className='text-center p-3',),
+    className='container-fluid',), className='fixed-bottom', )
+
+app.layout = html.Div([dcc.Location(id="url"), html.Div(id='page-content'), footer], style={ 'width' : '100%', 'height' : '100%',},) 
+
+#Routing for the mutiple pages
+@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
+def render_page_content(pathname):
+    if pathname == "/":
+        return dbc.Col([sidebar,home_updateLayout(),], style={ 'width' : '100%', 'height' : '100%',}, ),
+    elif pathname == "/page1":
+        return dbc.Col([sidebar,p1_updateLayout(),], style={ 'width' : '100%', 'height' : '100%',}, ),
+    elif pathname == "/page2":
+        return dbc.Col([sidebar,p2_updateLayout(),], style={ 'width' : '100%', 'height' : '100%',}, ),
+    elif pathname == "/page3":
+        return dbc.Col([sidebar,p3_updateLayout(),], style={ 'width' : '100%', 'height' : '100%',}, ),
+    elif pathname == "/page4":
+        return dbc.Col([sidebar,p4_updateLayout(),], style={ 'width' : '100%', 'height' : '100%',}, ),
+    elif pathname == "/page5":
+        return dbc.Col([sidebar,p5_updateLayout(),], style={ 'width' : '100%', 'height' : '100%',}, ),
+    # If the user tries to reach a different page, return a 404 message
+    return dbc.Jumbotron(
+        [
+            html.H1("404: Not found", className="text-danger"),
+            html.Hr(),
+            html.P(f"The pathname {pathname} was not recognised..."),
+        ])
+
+if __name__ == "__main__":
+    app.run_server()
---
+++ page1.py
@@ -0,0 +1,50 @@
+import dash
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+from dash.dependencies import Input, Output
+#==> import external method from .py file from folder /data,  wwhich is plotting the graph
+
+def p1_updateLayout():
+
+    #Defining Spaces ==> Insert your plot into the spaces
+    leftSpace = html.Div("Linker Space")
+        #Example : leftSpace = html.Div(Call_method_of_plotted_graph)
+    midSpace = html.Div("Mid Space")
+    rightSpace = html.Div("Rechter Space")
+
+    bot_leftSpace = html.Iframe( src = "https://datahub.io/core/glacier-mass-balance/view/0", style ={'width' : "100%", 'overflow' : 'hidden', 'height' : 475, 'frameborder' :0,} )
+    bot_midSpace = html.Div("Mid Space")
+    bot_rightSpace = html.Div("Rechter Space")
+
+    #In "content" the grid gets initialised and styled via HTML and CSS ==> If your graph doesent get displayed the right way you can adjust the styling or text Konstantin
+    content = html.Div(
+        [dbc.Row( [
+            dbc.Col(
+            [leftSpace, html.Div(
+                style={'width': '100%', 'height': 500, 'background-color' : '#33FFFC'},
+            )],className='col-2', style ={'padding':20}),
+            dbc.Col(
+            [midSpace, html.Div(
+                style={'width': '100%', 'height': 500, 'background-color' : '#888888'},
+            )], className='col-8',style ={'padding':20}),
+            dbc.Col(
+            [rightSpace, html.Div(
+                style={'width': '100%', 'height': 500, 'background-color' : '#888888'},
+            )], className='col-2', style ={'padding':20}),],
+            ),
+            dbc.Row( [
+            dbc.Col(
+            bot_leftSpace,className='col-4'),
+            dbc.Col(
+            [bot_midSpace, html.Div(
+                style={'width': '100%', 'height': 500, 'background-color' : '#888888'},
+            )], className='col-4', style ={'padding':20}),
+            dbc.Col(
+            [bot_rightSpace, html.Div(
+                style={'width': '100%', 'height': 500, 'background-color' : '#888888'},
+            )], className='col-4',style ={'padding':20}),],
+            )],
+            style={ 'width' : 'auto', 'padding' : 30, 'overflow' : 'hidden'},)
+    
+    return content
