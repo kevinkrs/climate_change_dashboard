f5fbc350e958994ea8e1043522bf5623b71d043a
KonstIT <merkichmirsicher@gmx.de>
2021-01-05 17:05:46 +0100

    Update#2

--- page4.py
+++ page4.py
@@ -3,18 +3,19 @@ import dash_html_components as html
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 from dash.dependencies import Input, Output
+from data.Economic_Impact.graphs import get_iGreenBondData, get_dropGDP, get_dropGDP_W
 #==> import external method from .py file from folder /data,  wwhich is plotting the graph
 
 def p4_updateLayout():
     #Defining Spaces ==> Insert your plot into the spaces
     leftSpace = html.Div("Linker Space")
         #Example : leftSpace = html.Div(Call_method_of_plotted_graph)
-    midSpace = html.Div("Mid Space")
+    midSpace = html.Div(dcc.Graph(figure=get_dropGDP_W()))
     rightSpace = html.Div("Rechter Space")
 
     bot_leftSpace = html.Iframe( src = "https://datahub.io/core/glacier-mass-balance/view/0", style ={'width' : "100%", 'overflow' : 'hidden', 'height' : 475, 'frameborder' :0,} )
-    bot_midSpace = html.Div("Mid Space")
-    bot_rightSpace = html.Div("Rechter Space")
+    bot_midSpace = html.Div(dcc.Graph(figure=get_dropGDP()))
+    bot_rightSpace = html.Div(dcc.Graph(figure=get_iGreenBondData()))
 
     #In "content" the grid gets initialised and styled via HTML and CSS ==> If your graph doesent get displayed the right way you can adjust the styling or text Konstantin
     content = html.Div(
@@ -24,9 +25,8 @@ def p4_updateLayout():
                 style={'width': '100%', 'height': 500, 'background-color' : '#FFFB00'},
             )],className='col-2', style ={'padding':20}),
             dbc.Col(
-            [midSpace, html.Div(
-                style={'width': '100%', 'height': 500, 'background-color' : '#888888'},
-            )], className='col-8',style ={'padding':20}),
+            midSpace,
+            className='col-8',style ={'padding':20}),
             dbc.Col(
             [rightSpace, html.Div(
                 style={'width': '100%', 'height': 500, 'background-color' : '#888888'},
@@ -36,13 +36,9 @@ def p4_updateLayout():
             dbc.Col(
             bot_leftSpace,className='col-4'),
             dbc.Col(
-            [bot_midSpace, html.Div(
-                style={'width': '100%', 'height': 500, 'background-color' : '#888888'},
-            )], className='col-4', style ={'padding':20}),
+            bot_midSpace, className='col-4', style ={'padding':20}),
             dbc.Col(
-            [bot_rightSpace, html.Div(
-                style={'width': '100%', 'height': 500, 'background-color' : '#888888'},
-            )], className='col-4',style ={'padding':20}),],
+            bot_rightSpace, className='col-4',style ={'padding':20}),],
             )],
             style={ 'width' : 'auto', 'padding' : 30, 'overflow' : 'hidden'},)
     
