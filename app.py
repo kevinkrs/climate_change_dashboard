ec18bb970bda6d1e21722674ee38f5f4198089cc
Issam Merikhi <47690579+IssamMerikhi@users.noreply.github.com>
2021-01-10 17:38:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -180,5 +180,18 @@ def update_output(selection):
     fig=get_worldMaps()[int(selection)]
     return fig
 
+
+#Call back for the pop up box
+@app.callback(
+    Output("modal", "is_open"),
+    [Input("open", "n_clicks"), Input("close", "n_clicks")],
+    [State("modal", "is_open")],
+)
+def toggle_modal(n1, n2, is_open):
+    if n1 or n2:
+        return not is_open
+    return is_open
+
+
 if __name__ == "__main__":
     app.run_server()
