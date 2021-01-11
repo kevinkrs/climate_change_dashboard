56cb853c08cd92249b4e75500588f641f39e01ea
Issam Merikhi <47690579+IssamMerikhi@users.noreply.github.com>
2021-01-11 19:10:16 +0100

    Add sidebar infobox

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ from page5 import p5_updateLayout
 from page5 import get_pie
 from data.Economic_Impact.graphs import get_dmgEU, get_dropGDP, get_worldMaps
 from info_box.infop4 import get_infoBox
+from info_box.infop5 import get_infoBox5
 
 from data.technology_patents.maps import *
 from data.technology_patents.graphs import *
@@ -51,6 +52,8 @@ sidebar = html.Div(
             pills=True,
                 ),
         html.Div(id='infoBox', className='infoFrame'),
+        html.Div(id='infoBox5', className='infoFrame'),
+
         
     ],
     className='sidebar', 
@@ -90,6 +93,13 @@ def render_page_content(pathname):
     [Input('url', 'pathname')])
 def callback_func(pathname):
     return get_infoBox(pathname)
+
+
+@app.callback(
+    Output('infoBox5', 'children'),
+    [Input('url', 'pathname')])
+def callback_func5(pathname):
+    return get_infoBox5(pathname)
 
 # Callback for different patent worldmaps 
 @app.callback(
