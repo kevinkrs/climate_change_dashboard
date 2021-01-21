a13c2dfac911ea299e7bfcdb496276f1d41c8a91
KonstIT <merkichmirsicher@gmx.de>
2021-01-21 16:08:08 +0100

    Fixes

--- data/Economic_Impact/graphs.py
+++ data/Economic_Impact/graphs.py
@@ -68,8 +68,8 @@ def get_dropGDP_W():
         fig = px.choropleth(df3, locations="CODE",
                     color="value", # lifeExp is a column of gapminder
                     hover_name="variable", # column to add to hover information
-                    color_continuous_scale=px.colors.sequential .OrRd[::-1],
-                    animation_frame='Date',
+                    #color_continuous_scale=px.colors.sequential .OrRd[::-1],
+                    #animation_frame='Date',
                     #range_color=[-4, 0]
                     )
         fig.update_layout(margin=dict(l=20,r=0,b=0,t=70,pad=0),paper_bgcolor="white",height= 700,title_text = 'Percentage change in regional GDP due to selected climate change impacts',font_size=18)
