db916147e210f7c7b8be5bce6738c9f6c0efb131
KonstIT <merkichmirsicher@gmx.de>
2021-01-22 19:28:22 +0100

    Alpha

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from lib.lib import *
-from app import app
 #Setup for Layout, seperate pages, etc.
 from home import home_updateLayout
 #from page1 import p1_updateLayout, renewable, energie, get_worldMaps_page_1_2, world_map_page1_1, world_map_page1_2, world_map_page1_3, get_worldMaps_page_1_1, temperature_page1
