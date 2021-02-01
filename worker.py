8c5a678c76a7c0b763617a25b313c3fce65a02ad
KonstIT <merkichmirsicher@gmx.de>
2021-02-01 18:49:47 +0100

    Test

--- worker.py
+++ worker.py
@@ -1,10 +1,10 @@
 from rq import Connection, Worker
 
-from app import conn, queue,enqueue_jobs_init
+from app import conn, queue
 
 if __name__ == "__main__":
     with Connection(conn):
         w = Worker([queue])
         w.work()
-    enqueue_jobs_init()
+
         
